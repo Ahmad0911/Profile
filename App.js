4400dd3ce6b07d0d26c4fe5312c60bb86f7f8b74
Ahmad0911 <83254452+Ahmad0911@users.noreply.github.com>
2022-09-07 10:04:50 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import Photo from "./Photo";
+import React from 'react';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+    <Photo />
+      <h2>Muhammed Ahmed</h2>
+      <p>Software Engineer, Nigeria.</p>
+      <h4>Skills</h4>
+      <div id="laugh">
+      <button>Html</button>
+      <button>Css</button>
+      <button>Scss</button>
+      <button>Js</button>
+      <button>React</button>
+      <button>Redux</button>
+      <button>Node</button>
+      <button>MongoDB</button>
+      <button>python</button>
+      <button>Flask</button>
+      <button>Django</button>
+      <button>Numpy</button>
+      <button>Pandas</button>
+      <button>Data Analysis</button>
+      <button>MySQL</button>
+      <button>GraphQL</button>
+      <button>D3.js</button>
+      <button>Gatsby</button>
+      <button>Docker</button>
+      <button>Heroku</button>
+      <button>Git</button>
+      </div>
+      <h6>Joined on Sept 3rd 2022</h6>
+    </div>
+  );
+}
+
+export default App;
---
+++ Photo.js
@@ -0,0 +1,12 @@
+
+import photo from './picture/photo.jpg';
+
+function Photo() {
+  return (
+    <div className="App">
+       <img src={photo}  alt='port icon' />
+    </div>
+  )
+}
+
+export default Photo;
